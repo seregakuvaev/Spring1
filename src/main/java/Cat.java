d2f04d95be7c3cc277b5856320aa3be0397650de
seregakuvaev <seregakuvaev@mail.ru>
2022-09-06 15:46:13 +0300

    new

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String catname;
+
+    public String getCatName() {
+        return catname;
+    }
+
+    public void setCatName(String name) {
+        this.catname = name;
+    }
+}
